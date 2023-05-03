30db0cfb65ae90f3836b39f0f148039deaa1faf8
wuhaixu2016 <wuhaix2016@163.com>
2023-05-03 12:37:27 +0800

    Update darcy_lsm.sh

--- scripts/darcy_lsm.sh
+++ scripts/darcy_lsm.sh
@@ -12,12 +12,12 @@ python exp_darcy.py \
   --h-down 5 \
   --w-down 5 \
   --batch-size 20 \
-  --learning-rate 0.001 \
+  --learning-rate 0.0005 \
   --model LSM_2D \
   --d-model 64 \
   --num-basis 12 \
   --num-token 4 \
-  --patch-size 3,3 \
+  --patch-size 6,6 \
   --padding 11,11 \
   --model-save-path ./checkpoints/darcy \
   --model-save-name lsm.pt
